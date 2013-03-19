c6042eceb0993fae489fd93a9dfa803c84d002e3
JustMoes <nick.smit@quicknet.nl>
2013-03-19 15:08:30 +0100

    Initial configuration file

---
+++ configuration.php
@@ -0,0 +1,13 @@
+<?PHP
+
+$cfg['PROJECT_NAME'] = 'projectManager';
+$cfg['PROJECT_DEVELOPER_NAME'] = 'Nick Smit';
+$cfg['PROJECT_DEVELOPER_MAIL'] = 'nicksmit1994@gmail.com';
+
+$cfg['PROJECT_DIRECTORY'] = 'projectManager/';
+
+$cfg['SMTP_SERVER'] = 'smtp.ziggo.nl';
+
+$cfg['PROJECT_MAIL_FROM'] = 'no.reply@projectmanager.nl';
+
+?>
