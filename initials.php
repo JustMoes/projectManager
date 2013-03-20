bb7f59eaecfe647d3b2fb3eab0bf7596a1dcb22b
JustMoes <nick.smit@quicknet.nl>
2013-03-20 08:54:32 +0100

    Added smarty constants

--- initials.php
+++ initials.php
@@ -4,6 +4,8 @@ define('MODELS', '/'. $cfg['PROJECT_DIRECTORY'] .'models/');
 define('CONTROLLERS', '/'. $cfg['PROJECT_DIRECTORY'] .'controllers/');
 define('VIEWS', '/'. $cfg['PROJECT_DIRECTORY'] .'views/');
 define('LIBS', '/'. $cfg['PROJECT_DIRECTORY'] .'libs/');
+define('TEMPLATES', '/'. $cfg['PROJECT_DIRECTORY'] .'html/templates/');
+define('C_TEMPLATES', '/'. $cfg['PROJECT_DIRECTORY'] .'html/c_templates/');
 
 define('ERROR_MAIL_NAME', $cfg['PROJECT_DEVELOPER_NAME']);
 define('ERROR_MAIL_TO', $cfg['PROJECT_DEVELOPER_MAIL']);
